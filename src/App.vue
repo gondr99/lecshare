14f662ac5640f394119aad5d1a857acbf15cf494
gondr <gondr99@gmail.com>
2019-03-20 11:16:42 +0900

    들여쓰기 정리

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,56 @@
 <template>
-  <div>
+<div>
     <div class="logo">
-      <h2>Gondr Lecture App. Ver 0.5</h2>
+        <h2>Gondr Lecture App. Ver 0.5</h2>
     </div>
     <div class="row">
-      <div class="col-3">
-        <ul class="nav flex-column nav-pills">
-          <a class="nav-link active">공유폴더 설정</a>
-          <a class="nav-link">접속자 목록보기</a>
-          <a class="nav-link">파일 업로드</a>
-          <a class="nav-link">권한 관리</a>
-        </ul>
-      </div>
-      <div class="col-9">        
-        <folder-component></folder-component>
-      </div>
+        <div class="col-3">
+            <ul class="nav flex-column nav-pills">
+                <a class="nav-link active">공유폴더 설정</a>
+                <a class="nav-link">접속자 목록보기</a>
+                <a class="nav-link">파일 업로드</a>
+                <a class="nav-link">권한 관리</a>
+            </ul>
+        </div>
+        <div class="col-9">
+            <folder-component v-show="mode == 0"></folder-component>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  mounted(){
-    document.addEventListener("keydown", (e)=>{
-      if(e.keyCode == 40 && e.ctrlKey){
-        this.openDevTools();
-      }
-    });
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    mounted() {
+        document.addEventListener("keydown", (e) => {
+            if (e.keyCode == 40 && e.ctrlKey) {
+                this.openDevTools();
+            }
+        });
+    },
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+			mode:0,
+        }
+    },
+    methods: {
+        openDevTools() {
+            let msg = this.$root.ipc.send("openDevTools", "이것이 메시지");
+        }
     }
-  },
-  methods:{
-    openDevTools(){
-      let msg = this.$root.ipc.send("openDevTools", "이것이 메시지");
-    }
-  }
 }
 </script>
 
 <style>
-  .logo {
+.logo {
     position: fixed;
-    left:10px;
-    bottom:10px;
-  }
+    left: 10px;
+    bottom: 10px;
+}
 
-  .logo > h2 {
-    font-size:14px;
-  }
+.logo>h2 {
+    font-size: 14px;
+}
 </style>
